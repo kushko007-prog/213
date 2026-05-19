75c6539097a8277dab6fa2a39dbfc04587fa3572
kushko007-prog <kushko007@gmail.com>
2026-05-19 19:35:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,62 +6,46 @@ from telegram.ext import (
     filters,
     ContextTypes,
 )
-
-import sqlite3
-
-
+import os
+import psycopg2
 
 TOKEN = "8931077914:AAH3sS6UTqy7uCb61YIms80hvnnuGOZknNo"
-
-
 ADMIN_ID = 7938699279
 
-
-
-
-conn = sqlite3.connect("students.db")
+# Подключение к PostgreSQL
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER,
+    user_id BIGINT PRIMARY KEY,
     name TEXT,
     group_name TEXT
 )
 """)
-
 conn.commit()
 
-
-
 schedule = {
-
     "ИС-21": """
 📚 Расписание группы ИС-21
-
 📘 Понедельник
 • Математика
 • Информатика
-
 📗 Вторник
 • Английский
 • Физика
 """,
-
     "П-14": """
 📚 Расписание группы П-14
-
 📘 Понедельник
 • История
 • Биология
-
 📗 Вторник
 • Химия
 • География
 """
 }
-
-
 
 keyboard = ReplyKeyboardMarkup(
     [
@@ -76,30 +60,19 @@ keyboard = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-
-
 waiting_for_registration = set()
 waiting_for_consultation = set()
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     user_id = update.message.from_user.id
-
     cursor.execute(
-        "SELECT * FROM users WHERE user_id=?",
+        "SELECT * FROM users WHERE user_id=%s",
         (user_id,)
     )
-
     user = cursor.fetchone()
-
-
-
     if not user:
-
         waiting_for_registration.add(user_id)
-
         await update.message.reply_text(
             "👋 Добро пожаловать!\n\n"
             "Для регистрации отправьте:\n\n"
@@ -107,255 +80,163 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "Пример:\n"
             "Иван ИС-21"
         )
-
     else:
-
         await update.message.reply_text(
             "👋 С возвращением!",
             reply_markup=keyboard
         )
 
 
-
 async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     user_id = update.message.from_user.id
     msg = update.message.text
 
-
-
     if user_id in waiting_for_registration:
-
         try:
-
             data = msg.split()
-
             name = data[0]
             group_name = data[1]
-
             cursor.execute(
-                "INSERT INTO users VALUES (?, ?, ?)",
+                "INSERT INTO users VALUES (%s, %s, %s)",
                 (user_id, name, group_name)
             )
-
             conn.commit()
-
             waiting_for_registration.remove(user_id)
-
             await update.message.reply_text(
                 f"✅ Регистрация завершена!\n\n"
                 f"👤 Имя: {name}\n"
                 f"📚 Группа: {group_name}",
                 reply_markup=keyboard
             )
-
         except:
-
             await update.message.reply_text(
                 "❌ Неверный формат.\n\n"
                 "Пример:\n"
                 "Иван ИС-21"
             )
-
         return
 
-
-
     if user_id in waiting_for_consultation:
-
         waiting_for_consultation.remove(user_id)
-
         await update.message.reply_text(
             "✅ Вы успешно записаны на консультацию!"
         )
-
-
-
         await context.bot.send_message(
             chat_id=ADMIN_ID,
             text=
             f"📥 Новая запись на консультацию!\n\n"
             f"{msg}"
         )
-
         return
 
-
-
     if msg == "📅 Расписание":
-
         cursor.execute(
-            "SELECT group_name FROM users WHERE user_id=?",
+            "SELECT group_name FROM users WHERE user_id=%s",
             (user_id,)
         )
-
         result = cursor.fetchone()
-
         if result:
-
             group_name = result[0]
-
             text = schedule.get(
                 group_name,
                 "❌ Расписание не найдено"
             )
-
             await update.message.reply_text(text)
-
-
-
     elif msg == "⏰ Ближайшая пара":
-
         await update.message.reply_text(
             "⏰ Следующая пара:\n\n"
             "📘 Математика\n"
             "🕒 10:00\n"
             "🏫 Кабинет 305"
         )
-
-
-
     elif msg == "📝 Домашка":
-
         await update.message.reply_text(
             "📝 Домашние задания:\n\n"
-            "📘 Математика — №25\n"
+            "📘 Математика — No25\n"
             "📗 История — реферат\n"
             "📙 Английский — стр. 15"
         )
-
-
-
     elif msg == "🏆 Оценки":
-
         await update.message.reply_text(
             "🏆 Ваши оценки:\n\n"
             "📘 Математика — 5\n"
             "📗 История — 4\n"
             "📙 Английский — 5"
         )
-
-
-
     elif msg == "👨‍🏫 Консультация":
-
         waiting_for_consultation.add(user_id)
-
         await update.message.reply_text(
             "📝 Отправьте одним сообщением:\n\n"
             "• ФИО\n"
             "• Предмет\n"
             "• Время"
         )
-
-
-
     elif msg == "📅 Экзамены":
-
         await update.message.reply_text(
             "📅 Ближайшие экзамены:\n\n"
             "📘 Математика — 25 июня\n"
             "📗 Физика — 28 июня"
         )
-
-
-
     elif msg == "📚 Материалы":
-
         await update.message.reply_text(
             "📚 Учебные материалы:\n\n"
             "📄 Лекции\n"
             "📄 Презентации\n"
             "📄 Методички"
         )
-
-
-
     elif msg == "📢 Новости":
-
         await update.message.reply_text(
             "📢 Новости колледжа:\n\n"
             "🎉 День студента — 20 мая\n"
             "📚 Сессия начинается 1 июня"
         )
-
-
-
     elif msg == "💰 Стипендия":
-
         await update.message.reply_text(
             "💰 Стипендии:\n\n"
             "📌 Обычная — 5000₽\n"
             "📌 Повышенная — 9000₽"
         )
-
-
-
     elif msg == "🏫 Кабинеты":
-
         await update.message.reply_text(
             "🏫 Кабинеты:\n\n"
             "305 — Математика\n"
             "204 — Деканат\n"
             "101 — Библиотека"
         )
-
-
-
     elif msg == "👤 Мой профиль":
-
         cursor.execute(
-            "SELECT name, group_name FROM users WHERE user_id=?",
+            "SELECT name, group_name FROM users WHERE user_id=%s",
             (user_id,)
         )
-
         user = cursor.fetchone()
-
         if user:
-
             await update.message.reply_text(
                 f"👤 Имя: {user[0]}\n"
                 f"📚 Группа: {user[1]}"
             )
-
-
-
     elif msg == "📞 Контакты":
-
         await update.message.reply_text(
             "📞 Контакты:\n\n"
             "☎️ Деканат: +7 8662 49-61-51\n"
             "📧 https://nalchik.top-academy.ru/"
         )
-
-
-
     elif msg == "❓ Помощь":
-
         await update.message.reply_text(
             "📞 Обратитесь в деканат."
         )
-
-
-
     else:
-
         await update.message.reply_text(
             "❗ Используйте кнопки меню"
         )
 
 
-
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(
     CommandHandler("start", start)
 )
-
 app.add_handler(
     MessageHandler(filters.TEXT, buttons)
 )
 
 print("✅ Бот запущен...")
-
 app.run_polling()
